107f24ee1c6dda3406755bc72703bdc61f6e0acf
Abhishek kumar <abhi.arya97161@gmail.com>
2024-04-08 13:38:22 +0530

    get image route added and working successfully

---
+++ controllers/Templete/getImage.js
@@ -0,0 +1,58 @@
+const fs = require("fs");
+const path = require("path");
+const Files = require("../../models/TempleteModel/files");
+const AdmZip = require("adm-zip");
+
+const getImage = async (req, res, next) => {
+  const fileId = req.params.id;
+  const { image } = req.body;
+  //   console.log(image, id);
+
+  try {
+    if (!fileId || !image) {
+      return res
+        .status(400)
+        .json({ error: "File ID or image name not provided" });
+    }
+
+    const fileData = await Files.findOne({ where: { id: fileId } });
+    if (!fileData) {
+      return res.status(404).json({ error: "File not found" });
+    }
+
+    const filename = fileData.zipFile;
+    const zipFilePath = path.join(__dirname, "../../zipFile", filename);
+
+    if (!fs.existsSync(zipFilePath)) {
+      return res.status(404).json({ error: "Zip file not found" });
+    }
+
+    const zip = new AdmZip(zipFilePath);
+    const zipEntries = zip.getEntries();
+    const matchingEntry = zipEntries.find((entry) =>
+      entry.entryName.includes(image)
+    );
+
+    if (!matchingEntry) {
+      return res.status(404).json({ error: "Image not found in the zip file" });
+    }
+
+    const imageData = zip.readFile(matchingEntry);
+    res.writeHead(200, {
+      "Content-Type": "image/tiff", // You may need to adjust the content type based on the image format
+      "Content-Length": imageData.length,
+    });
+
+    // const imageDataBuffer = Buffer.from(imageData);
+    // const base64Image = imageDataBuffer.toString("base64");
+    // const dataUrl = `data:image/jpeg;base64,${base64Image}`;
+    // console.log(imageData);
+
+    res.end(imageData);
+  } catch (error) {
+    console.error("Error handling data:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
+
+module.exports = getImage;
